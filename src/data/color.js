615020b6cdebd4a6b5a43ddea13d66f4c43b0bbd
anabelena <anabelen.aristah@gmail.com>
2025-01-29 11:00:42 -0800

    feat: add color and size data/objects

---
+++ src/data/color.js
@@ -0,0 +1,13 @@
+const colors = [
+    { name: "red", hex: "#FF0000" },
+    { name: "blue", hex: "#0000FF" },
+    { name: "green", hex: "#008000" },
+    { name: "yellow", hex: "#FFFF00" },
+    { name: "purple", hex: "#800080" },
+    { name: "orange", hex: "#FFA500" },
+    { name: "black", hex: "#000000" },
+    { name: "white", hex: "#FFFFFF" }
+  ];
+  
+  export default colors;
+  
---
+++ src/data/size.js
@@ -0,0 +1,11 @@
+const sizes = [
+    { size: "XS", description: "Extra Small" },
+    { size: "S", description: "Small" },
+    { size: "M", description: "Medium" },
+    { size: "L", description: "Large" },
+    { size: "XL", description: "Extra Large" },
+    { size: "XXL", description: "Double Extra Large" }
+  ];
+  
+  export default sizes;
+  
